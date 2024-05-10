377898948798fceebf33b6c37aafd3a51294fde2
Sergei <61847373+Lolozius@users.noreply.github.com>
2024-05-10 19:35:09 +0300

    Sample template

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-import cv2 as cv
-img = cv.imread('img.png')
+import cv2
 
-cv.imshow("Display window", img)
-k = cv.waitKey(0) # Wait for a keystroke in the window
+# Загрузка каскада Хаара для распознавания лиц
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Загрузка изображения
+image = cv2.imread('img_1.png')
+
+# Преобразование изображения в оттенки серого
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Обнаружение лиц на изображении
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6, minSize=(10, 10))
+
+# Отрисовка прямоугольников вокруг обнаруженных лиц
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+# Отображение результата
+cv2.imshow('Face Detection', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
